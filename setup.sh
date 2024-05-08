a757b16c5af36587088374fd3df7f32efa421341
Ken Lai <ken.lai212@gmail.com>
2024-05-08 22:50:16 +0800

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+apt update
+apt install python3
+apt install python3-pip
+pip install -r requirements.txt
+python3 -m nltk.download stopwords
+python3 -m nltk.download punkt
+wget https://ftp.ncbi.nlm.nih.gov/pub/lu/Suppl/BioSentVec/BioSentVec_PubMed_MIMICIII-bigram_d700.bin
